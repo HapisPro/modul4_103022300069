e085d201d06691dafaee763c7a202417a6544e4e
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 06:59:03 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalmod4_103022300069
+{
+    class KodeProduk
+    {
+        public enum produkElektronik {Laptop, Smartphone, Tablet, Headset, Keyboard, Mouse, Printer, Monitor, Smartwatch, Kamera };
+
+        public static string getKodeProduk(produkElektronik elektronik)
+        {
+            string[] kodeProduk = { "E100", "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109" };
+
+            return kodeProduk[(int)elektronik];
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
-
+using jurnalmod4_103022300069;
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("Soal 1----");
+        foreach(KodeProduk.produkElektronik elk in Enum.GetValues(typeof(KodeProduk.produkElektronik)))
+        {
+            Console.WriteLine(elk + ": " + KodeProduk.getKodeProduk(elk));
+        }
     }
 }
